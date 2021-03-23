7d98531203e53ce0cf053634762823648eb74b97
RoiEf <roi.efrati@gmail.com>
2021-03-23 22:12:32 +0200

    two objects

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -2,13 +2,18 @@ import { h } from 'preact';
 import style from './style.css';
 import Canvas from "../../components/canvas";
 
+
+let arr = [{x:50, y:50},{x:100,y:100}] ;
+
 const Home = () => {
 	const draw = (ctx, frameCount) => {
 		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 		ctx.fillStyle = "#000000";
-		ctx.beginPath();
-		ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
-		ctx.fill();
+		arr.forEach(element => {
+			ctx.beginPath();
+			ctx.arc(element.x, element.y, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
+			ctx.fill();
+		});
 	  };
 
 	  return (
